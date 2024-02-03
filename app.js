fa3fbf9bada42eab28cc3b944977ea7931db1f22
Davi Costa <davikosta2003@gmail.com>
2024-02-03 17:33:10 -0300

    Melhoria na legibilidade do código e no comportamento dos botões copiar e reverter

--- app.js
+++ app.js
@@ -1,25 +1,92 @@
-let textoCopiado = false
 
 var entradaDeTexto = document.querySelector("#entrada-de-texto");
 
 var saidaDeTexto = document.querySelector("#saida-de-texto");
+//variável para manipular a mensagem que será exibida no botão copiar
+var textoCopiado = false;
+//função que define qual mensagem o botão de copiar irá exibir
+function exibirTextoCopiado() {
+    if (textoCopiado == true) {
+        document.getElementById("botao-copiar").innerText = "Copiado!";
+    } else {
+        document.getElementById("botao-copiar").innerText = "Copiar!";
+    }
+}
+//declaração de funcões para desabilitar botões da área de saída de texto
+function desabilitarBotaoReverter() {
+    document.getElementById("botao-reverter").disabled = true;
+}
 
+function desabilitarBotaoCopiar() {
+    document.getElementById("botao-copiar").disabled = true;
+}
+//chamado das funções para desabilitar botões da área de saída de texto
+desabilitarBotaoCopiar();
+
+desabilitarBotaoReverter();
+//declaração das funções para habilitar botões da área de saída de texto
+function habilitarBotaoReverter() {
+    document.getElementById("botao-reverter").disabled = false;
+}
+
+function habilitarBotaoCopiar() {
+    document.getElementById("botao-copiar").disabled = false;
+}
+//declaração de função para apagar a entrada de texto
+function apagarEntrada() {
+    document.getElementById("entrada-de-texto").value = "";
+}
+
+
+//declaração das funções de codificar e decodificar
 function codificar() {
-    let entrada = entradaDeTexto.value;
-    
-    let resultado = entrada.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+    //verificação afim de evitar tentativas com a entrada de texto vazia
+    if (entradaDeTexto.value == "") {
+        document.getElementById("saida-de-texto").innerHTML = "Insira um texto ou palavra!"
+    } else {
+        let entrada = entradaDeTexto.value;
 
-    document.getElementById("saida-de-texto").innerHTML = resultado;
+        let resultado = entrada.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
 
-    textoCopiado = false;
+        document.getElementById("saida-de-texto").innerHTML = resultado;
 
-    exibirTextoCopiado()
+        textoCopiado = false;
 
-    apagarEntrada()
+        exibirTextoCopiado();
+
+        apagarEntrada();
+
+        habilitarBotaoCopiar();
+
+        habilitarBotaoReverter();
+    }
 }
 
 function decodificar() {
-    let entrada = entradaDeTexto.value;
+    //verificação afim de evitar tentativas com a entrada de texto vazia
+    if (entradaDeTexto.value == "") {
+        document.getElementById("saida-de-texto").innerHTML = "Insira um texto ou palavra!";
+    } else {
+        let entrada = entradaDeTexto.value;
+
+        let resultado = entrada.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+
+        document.getElementById("saida-de-texto").innerHTML = resultado;
+
+        textoCopiado = false;
+
+        exibirTextoCopiado();
+
+        apagarEntrada();
+
+        habilitarBotaoCopiar();
+
+        desabilitarBotaoReverter();
+    }
+}
+//função para reverter codificação
+function reverter() {
+    let entrada = saidaDeTexto.value;
 
     let resultado = entrada.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
 
@@ -29,21 +96,9 @@ function decodificar() {
 
     exibirTextoCopiado();
 
-    apagarEntrada()
+    desabilitarBotaoReverter();
 }
-
-function reverterdecodificar() {
-    let entrada = saidaDeTexto.value;
-
-    let resultado = entrada.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
-
-    document.getElementById("saida-de-texto").innerHTML = resultado;
-
-    textoCopiado = false;
-
-    exibirTextoCopiado();   
-}
-
+//função para copiar o resultado da codificação/decodificação
 function copiar() {
     let copiarTexto = document.getElementById("saida-de-texto");
 
@@ -54,16 +109,6 @@ function copiar() {
     textoCopiado = true;
 
     exibirTextoCopiado();
-}
 
-function apagarEntrada() {
-    document.getElementById("entrada-de-texto").value = "";
-}
-//função que define qual mensagem o botão de copiar irá exibir
-function exibirTextoCopiado() {
-    if(textoCopiado == true) {
-        document.getElementById("botao-copiar").innerText = "Copiado!";
-    } else {
-        document.getElementById("botao-copiar").innerText = "Copiar!";
-    }
+    desabilitarBotaoCopiar();
 }
